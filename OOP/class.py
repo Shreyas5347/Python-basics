9fbc8c6f96bec5a3f0d2f3917cd4b6d03fa048b7
Shreyas5347 <rajputshreyas321@gmail.com>
2026-03-26 16:43:19 +0530

    about class,reference variable and object

---
+++ OOP/class.py
@@ -0,0 +1,7 @@
+class Car:
+  color = "red"
+  brand = "Toyota"
+c1=Car() #object creation
+print(c1.color)
+#here c1 is a reference variable and Car() is an object of the class Car
+#class is a blueprint and object is an instance of the class
