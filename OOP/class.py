e9d2901646dca13c613df3639f8856362a45bcfe
Shreyas5347 <rajputshreyas321@gmail.com>
2026-04-06 19:08:44 +0530

    about class,object and reference variable

--- OOP/class.py
+++ OOP/class.py
@@ -1,3 +1,5 @@
+#class is a blueprint and object is an instance of the class
+#Class variables like color,brand are shared by all objects of the class
 class Car:
   color = "red"
   brand = "Toyota"
@@ -5,5 +7,3 @@ c1=Car() #object creation(memory allocated)
 print(c1.color)
 #here c1 is a reference variable and Car() is an object of the class Car
 #A reference variable stores the address (reference) of an object, not the actual object
-#class is a blueprint and object is an instance of the class
-#Class variables like color,brand are shared by all objects of the class
